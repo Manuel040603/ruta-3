14a25096ca82445b9769111e84dfeb788cc997da
Manuel040603 <manuel.torresc@unmsm.edu.pe>
2026-08-30 22:03:05 -0500

    Update MainActivity.kt

--- ComposeArticle/MainActivity.kt
+++ ComposeArticle/MainActivity.kt
@@ -1,39 +1,39 @@
-package com.example.composequadrant
+package com.example.composearticle
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
+import androidx.compose.ui.unit.sp
+import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeQuadrantTheme {
+            ComposeArticleTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ComposeQuadrantScreen(modifier = Modifier.fillMaxSize())
+                    ComposeArticleScreen(modifier = Modifier.fillMaxSize())
                 }
             }
         }
@@ -41,70 +41,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposeQuadrantScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Row(modifier = Modifier.weight(1f)) {
-            ComposeQuadrant(
-                title = "Text composable",
-                description = "Displays text and follows the recommended Material Design guidelines.",
-                backgroundColor = Color(0xFFEADDFF),
-                modifier = Modifier.weight(1f)
-            )
-            ComposeQuadrant(
-                title = "Image composable",
-                description = "Creates a composable that lays out and draws a given Painter class object.",
-                backgroundColor = Color(0xFFD0BCFF),
-                modifier = Modifier.weight(1f)
-            )
-        }
-        Row(modifier = Modifier.weight(1f)) {
-            ComposeQuadrant(
-                title = "Row composable",
-                description = "A layout composable that places its children in a horizontal sequence.",
-                backgroundColor = Color(0xFFB69DF8),
-                modifier = Modifier.weight(1f)
-            )
-            ComposeQuadrant(
-                title = "Column composable",
-                description = "A layout composable that places its children in a vertical sequence.",
-                backgroundColor = Color(0xFFF6EDFF),
-                modifier = Modifier.weight(1f)
-            )
-        }
-    }
-}
+fun ComposeArticleScreen(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    val scrollState = rememberScrollState()
 
-@Composable
-fun ComposeQuadrant(
-    title: String,
-    description: String,
-    backgroundColor: Color,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(backgroundColor)
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = title,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
+    Column(modifier = modifier.verticalScroll(scrollState)) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier.fillMaxWidth()
         )
         Text(
-            text = description,
-            textAlign = TextAlign.Justify
+            text = stringResource(R.string.compose_title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.compose_description_1),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.compose_description_2),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun ComposeQuadrantPreview() {
-    ComposeQuadrantTheme {
-        ComposeQuadrantScreen()
+fun ComposeArticlePreview() {
+    ComposeArticleTheme {
+        ComposeArticleScreen()
     }
 }
